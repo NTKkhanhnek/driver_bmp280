722037e60be41420ff0dbb9b7097830fe3dc5c2e
NTKkhanhnek <ntkkhanhnek06@gmail.com>
2026-07-18 22:52:49 +0700

    Refactor BMP280 initialization and error handling
    Removed error handling and simplified BMP280 initialization.

--- Src/main.c
+++ Src/main.c
@@ -6,44 +6,21 @@ volatile float temperature = 0.0f;
 volatile float pressure = 0.0f;
 volatile float altitude = 0.0f;
 
-volatile BMP280_StatusTypeDef bmp280_status = BMP280_ERROR;
-
-static void app_error_handler(void)
-{
-    while (1)
-    {
-    }
-}
-
 int main(void)
 {
-    rcc_init();
+    clock_init();
     delay_init(RCC_SYS_CLOCK_HZ);
-
-    bmp280_status = BMP280_Init();
-    if (bmp280_status != BMP280_OK)
-    {
-        app_error_handler();
-    }
-
-    bmp280_status = BMP280_SetReferencePressure();
-    if (bmp280_status != BMP280_OK)
-    {
-        app_error_handler();
-    }
-    delay_ms(2000);
+    BMP280_Init();
+    delay_ms(20000);
+    BMP280_SetReferencePressure();
     while (1)
     {
         float current_temperature = 0.0f;
         float current_pressure = 0.0f;
         float current_altitude = 0.0f;
 
-        bmp280_status = BMP280_ReadTemperatureAndPressureAndAltitude(&current_temperature, &current_pressure, &current_altitude);
-        if (bmp280_status != BMP280_OK)
-        {
-            app_error_handler();
-        }
-
+        BMP280_ReadTemperatureAndPressureAndAltitude(&current_temperature, &current_pressure, &current_altitude);
+        
         temperature = current_temperature;
         pressure = current_pressure;
         altitude = current_altitude;
